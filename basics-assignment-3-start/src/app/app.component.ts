81742a8dc78a8f603fb437d5c69f9f921010a652
deanzaka <deanzaka.h@gmail.com>
2017-08-02 16:48:53 +0700

    finish basic assignment 3

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  testIf = false;
+  items = [];
+
+  onButtonPressed() {
+    this.testIf = !this.testIf;
+    // this.items.push(this.items.length+1);
+    this.items.push(new Date);
+  }
 }
